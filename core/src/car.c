52de367d637064b0d9a9552c654c09007b50f3e4
Tarek M. <contact@mechkenetarek.com>
2025-04-30 16:25:01 +0200

    ADD - Slower logs with decreasing size.

--- core/src/car.c
+++ core/src/car.c
@@ -1,32 +1,50 @@
 #include "car.h"
 #include <stdlib.h>
 
+void determineMaxesValues(int score, Occupation roadType, int *maxSize, int *maxSpeed, int *minSpeed, int *maxCars) {
+    if (roadType == ROAD) {
+        *maxSize = (score / 20 + 3 >= 6) ? 6 : score / 20 + 3;
+        *minSpeed = (6 - score / 25 < 2) ? 2 : 6 - score / 25;
+        *maxSpeed = *minSpeed + 3;
+        *maxCars = (score >= 100) ? 4 : 2 + score / 20;
+    } else if (roadType == WATER) {
+        *maxSize = (6 - score / 20 < 3) ? 3 : 6 - score / 20;
+        *minSpeed = (7 - score / 25 < 4) ? 4 : 7 - score / 25;
+        *maxSpeed = *minSpeed + 1;
+        *maxCars = (4 - score / 30 < 2) ? 2 : 4 - score / 30;
+    }
+
+    if (maxSpeed < minSpeed) maxSpeed = minSpeed;
+}
+
 void addCar(GameState *gs, int y, int forcedDirection, int availableSize, Occupation roadType)
 {
-    if (y < 0 || y >= gs->grid->height) return;
-    if (gs->grid->cases[y][0] == SAFE || gs->grid->cases[y][0] == TREE) return;
+    if (y < 0 || y >= gs->grid->height)
+        return;
+    if (gs->grid->cases[y][0] == SAFE || gs->grid->cases[y][0] == TREE)
+        return;
 
     Car *baseCar = (Car *)malloc(sizeof(Car));
-    if (!baseCar) return;
+    if (!baseCar)
+        return;
 
     int direction = forcedDirection == 0 ? (rand() % 2 ? 1 : -1) : forcedDirection;
     int startingX = (direction == 1) ? 0 : gs->grid->length - 1;
 
-    int maxSize = (gs->score / 20 + 3 >= 6) ? 6 : gs->score / 20 + 3;
+    int maxSize, maxSpeed, maxCars, minSpeed;
+    
+    determineMaxesValues(gs->score, roadType, &maxSize, &maxSpeed, &minSpeed, &maxCars);
+
     int size = 1 + rand() % (maxSize < availableSize ? maxSize : availableSize);
-
-    int maxSpeed = (gs->score / 20 + 1 >= 5) ? 5 : gs->score / 20 + 1;
-    int speed = 1 + rand() % maxSpeed;
-
-    int maxCars = (gs->score >= 100) ? 4 : 2 + gs->score / 20;
+    int speed = minSpeed + rand() % (maxSpeed - minSpeed + 1);
     int desiredCars = 1 + rand() % maxCars;
-
     int loopTime = gs->grid->length * speed;
 
-    *baseCar = (Car){.x = startingX, .y = y, .size = size, .direction = direction, .speed = speed, .accumulator = 0, .type = roadType}; 
-    
-    //roadType : When we have ROAD, then we know for sure that we have cars. When we have WATER, we know we have logs.
-    gs->grid->cases[y][startingX] = roadType == WATER ? LOG : direction == 1 ? CAR_RIGHT : CAR_LEFT;;
+    *baseCar = (Car){.x = startingX, .y = y, .size = size, .direction = direction, .speed = speed, .accumulator = 0, .type = roadType};
+
+    // roadType : When we have ROAD, then we know for sure that we have cars. When we have WATER, we know we have logs.
+    gs->grid->cases[y][startingX] = roadType == WATER ? LOG : direction == 1 ? CAR_RIGHT
+                                                                             : CAR_LEFT;
     addLastCar(gs->cars, baseCar);
 
     availableSize -= size;
@@ -47,12 +65,14 @@ void addCar(GameState *gs, int y, int forcedDirection, int availableSize, Occupa
             break;
 
         Car *nextCar = (Car *)malloc(sizeof(Car));
-        if (!nextCar) return;
+        if (!nextCar)
+            return;
 
         *nextCar = (Car){.x = startingX, .y = y, .size = nextSize, .direction = direction, .speed = speed, .accumulator = 0};
 
         Effect *e = (Effect *)malloc(sizeof(Effect));
-        if (!e) {
+        if (!e)
+        {
             free(nextCar);
             return;
         }
@@ -77,9 +97,9 @@ void updateCars(GameState *gs)
         Car *c = cursor->car;
         CarElement *next = cursor->next;
 
-        // The car's accumulator is a number that once reached will make the car move forward. It is then resets. The speed is the number of accumulating points given to a car per Frame.
-        // Therefore, a car with an accumulator or 100, a speed of 2, will move forward every 50 frames. In 60 FPS, it is 1.2 seconds.
-        // Formula is : FPS/(Accumulator/Speed). Higher FPS = Slower cars, unless the accumulator is scalled down/speed is scalled up.
+        // The car's accumulator will climb with each frame incrementing it. Once it reaches speed, it will reset and make the car moves forward in the same frame.
+        // e.g.: A car with a speed of 10 will need 10 frames to move forward, so in 60 FPS he will move forward every 1/6 second, making him moves forward 6 times per second. 
+        // Formula is : FPS/Speed.
 
         if (c->accumulator == c->speed)
         {
@@ -92,12 +112,12 @@ void updateCars(GameState *gs)
             c->accumulator = 0;
 
             if (c->type == WATER && gs->player->y == c->y) // If player is on water, game should stop, unless he is on a log.
-                gs->player->x += c->direction; // On a log, the player moves too.
-            
+                gs->player->x += c->direction;             // On a log, the player moves too.
 
             for (int i = 0; i < c->size; i++) // Put the car at his new position
                 if (c->y >= 0 && c->y < gs->grid->height && c->x + i * c->direction >= 0 && c->x + i * c->direction < gs->grid->length)
-                    gs->grid->cases[c->y][c->x + i * c->direction] = c->type == WATER ? LOG : c->direction == 1 ? CAR_RIGHT : CAR_LEFT;
+                    gs->grid->cases[c->y][c->x + i * c->direction] = c->type == WATER ? LOG : c->direction == 1 ? CAR_RIGHT
+                                                                                                                : CAR_LEFT;
         }
         else
         {
